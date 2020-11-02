61ad9fb35bd5688a8fab8e49208c7e03925e17d5
steven payne <djnosey@hotmail.co.uk>
2020-11-02 12:49:07 +0100

    start work on sign up

---
+++ src/User.js
@@ -0,0 +1,10 @@
+"use strict"
+
+class User {
+    constructor(name, email, password, repeatedpassword){
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.repeatedpassword = repeatedpassword;
+    }
+}
--- src/signup.js
+++ src/signup.js
@@ -5,7 +5,7 @@ class SignUp {
     //inputs
 
     this.nameInput = document.querySelector("#name");
-    this.EmailInput = document.querySelector("#email");
+    this.emailInput = document.querySelector("#email");
     this.passwordInput = document.querySelector("#password");
     this.repeatPasswordInput = document.querySelector("#repeatpassword");
 
@@ -15,17 +15,45 @@ class SignUp {
 
   //handle the inputs
 
-  handleEmailInput = () => {};
-  handlePasswordInput = () => {};
-  handleRepeatPasswordInput = () => {};
+  handleEmailInput = (event) => {
+    const emailInput = event.target;
+    const email = emailInput.value;
+  };
+  handlePasswordInput = (event) => {
+    const passwordInput = event.target;
+    const password = passwordInput.value;
+  };
+  handleRepeatPasswordInput = (event) => {
+    const repeatPasswordInput = event.target;
+    const repeatPassword = repeatPasswordInput.value;
+  };
 
   //handle the sending of data on submit
-  saveData = () => {};
+  saveData = (event) => {
+    event.preventDefault();
+
+    const name = this.nameInput.value;
+    const email = this.emailInput.value;
+    const password = this.passwordInput.value;
+    const repeatedPassword = this.repeatPasswordInput.value;
+
+    const newUser = new User(name, email, password, repeatedPassword);
+
+    console.log(newUser);
+
+    //save to db >>>>
+
+    //clear the form
+    this.nameInput.value = "";
+    this.emailInput.value = "";
+    this.passwordInput.value = "";
+    this.repeatPasswordInput.input = "";
+  };
 
   //event listeners
 
   addlisteners = () => {
-    this.EmailInput.addEventListener("input", this.handleEmailInput);
+    this.emailInput.addEventListener("input", this.handleEmailInput);
     this.passwordInput.addEventListener("input", this.handlePasswordInput);
     this.repeatPasswordInput.addEventListener(
       "input",
@@ -35,4 +63,7 @@ class SignUp {
   };
 }
 
+//creates an instance of the class
 const signUp = new SignUp();
+
+window.addEventListener("load", signUp.addlisteners);
